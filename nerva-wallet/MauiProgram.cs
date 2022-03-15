4a40759dca31e5f0adf517832762d4179850e4e8
R0BC0D3R <R0BC0D3R@gmail.com>
2022-03-14 21:21:51 -0400

    Start with maui template

---
+++ nerva-wallet/MauiProgram.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using nerva_wallet.Data;
+
+namespace nerva_wallet;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.RegisterBlazorMauiWebView()
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddBlazorWebView();
+		builder.Services.AddSingleton<WeatherForecastService>();
+
+		return builder.Build();
+	}
+}
